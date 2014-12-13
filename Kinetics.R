f24a0c72e9d4b7fca6d9ce005db1f3bce7b393f2
Kirsten <kirstenfrank25@yahoo.com>
2014-12-13 13:12:31 -0500

    Found maximum blood concentration and time of maximum blood concentration

--- Kinetics.R
+++ Kinetics.R
@@ -1,2 +1,17 @@
 data(Theoph)
 library(dplyr)
+
+## find the maximum blood concentration
+Theoph<-tbl_df(Theoph)
+Theo_grp_subject<-group_by(Theoph,Subject)
+Max_dose<-summarize(Theo_grp_subject,max(conc))
+names(Max_dose)<-c("Subject", "MaxConc")
+Max_dose<-arrange(Max_dose,Subject)
+Theo_grp_subject<-merge(Theo_grp_subject,Max_dose, by="Subject")
+
+## find the time at maximum blood concentration
+MaxTime<-filter(Theo_grp_subject,conc==MaxConc)
+MaxTime<-select(MaxTime, Subject, Time)
+MaxTime<-arrange(MaxTime,Subject)
+names(MaxTime)<-c("Subject","TimeAtMax")
+Theo_grp_Subject<-merge(Theo_grp_subject,MaxTime,by="Subject")
