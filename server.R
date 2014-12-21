0eedc08b5fe0d690ce660041de33805f2051630e
Kirsten <kirstenfrank25@yahoo.com>
2014-12-20 22:22:24 -0500

    prediction code is in server.R--but still working on formatting newdata

--- server.R
+++ server.R
@@ -1,7 +1,34 @@
 library(shiny)
+data(Theoph)
+library(dplyr)
+
+## find the maximum blood concentration
+Theoph<-tbl_df(Theoph)
+Theo_grp_subject<-group_by(Theoph,Subject)
+Max_dose<-summarize(Theo_grp_subject,max(conc))
+names(Max_dose)<-c("Subject", "MaxConc")
+Max_dose<-arrange(Max_dose,Subject)
+Theo_grp_subject<-merge(Theo_grp_subject,Max_dose, by="Subject")
+
+## find the time at maximum blood concentration
+MaxTime<-filter(Theo_grp_subject,conc==MaxConc)
+MaxTime<-select(MaxTime, Subject, Time)
+MaxTime<-arrange(MaxTime,Subject)
+names(MaxTime)<-c("Subject","TimeAtMax")
+Theo_grp_subject<-merge(Theo_grp_subject,MaxTime,by="Subject")
+
+
+## Simplify and fit
+MaxConcset<-filter(Theo_grp_subject,conc==MaxConc)
+MaxConcset<-select(MaxConcset,Subject,Wt,Dose,MaxConc,TimeAtMax)
+MaxConcset<-mutate(MaxConcset,recipWt = 1/Wt)
+## The best fit is one with interactions allowed between recipWt (1/Wt)
+## and Dose. 
+fitobj4<-lm(MaxConc~recipWt * Dose, MaxConcset)
 
 predictMaxDose<-function(Dose,Wt) {
-    predict(fitobj4, Dose=Dose, recipWt=1/Wt,interval="prediction")
+    newdata<-as.data.frame(c(1,Wt,Dose,0.0, 0.0,1/Wt))
+    predict(fitobj4, newdata,interval="prediction")
 }
 shinyServer(
     function(input,output) {
--- ui.R
+++ ui.R
@@ -3,14 +3,16 @@ shinyUI(pageWithSidebar(
     headerPanel("Predict Maximum Dose of Theophylline"),
     sidebarPanel(
         h3('Input values dose in mg/kg and weight in kg'),
-        numericInput('Dose', 'Dose in mg/kg', 0, min=0, max=10, step=0.1),
-        numericInput('Wt', 'Weight in kg',0, min=0, max=150, step=10)
+        numericInput('Dose', 'Dose in mg/kg', 5, min=0, max=10, step=0.1),
+        numericInput('Wt', 'Weight in kg',70, min=0, max=150, step=10)
         ),
     mainPanel(
         h3('Check Input values'),
         h4('You entered'),
         verbatimTextOutput("oDose"),
         h4('You entered'),
-        verbatimTextOutput("oWt")
+        verbatimTextOutput("oWt"),
+        h4('Your maximum dose range'),
+        verbatimTextOutput("results")
         )
     ))
